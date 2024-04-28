382b8d5ddc1215f177acade1ed64893e18bfb4a9
DataAmperes <jan.amhir.ventanilla956@adamson.edu.ph>
2024-04-28 16:36:48 +0800

    Jian

---
+++ resources/views/events.blade.php
@@ -0,0 +1,101 @@
+
+<head>
+@include('header')
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title></title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+<div class="container-sm my-5">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"></script>
+    <div class="carousel slide" id="carouseltest"
+    data-bs-wrap="true"
+    data-bs-slide="carousel">
+   
+
+ <div class="carousel-inner">
+
+ <div class="carousel-item active"
+ data-bs-interval="2000">
+ <div class="d-flex justify-content-center">
+    <img src="images\Guardians of life.jpg" class="w-35">
+</div>
+ </div>
+
+ <div class="carousel-item"
+ data-bs-interval="2000">
+ <div class="d-flex justify-content-center">
+    <img src="images\catto donations.jpg" class="w-50">
+    <div class="carousel-caption d-none d-md-block">
+        <button class="btn btn-primary" href="{{ url('https://www.facebook.com/groups/946027585595117') }}">Redirect</button>
+    </div>
+</div>
+ </div>
+
+ <div class="carousel-item"
+ data-bs-interval="2000">
+ <div class="d-flex justify-content-center">
+    <img src="images\cute-feline-cate.jpg" class="w-75">
+</div>
+ </div>
+
+ </div>
+<button class="carousel-control-prev" type="button"
+data-bs-target="#carouseltest"
+data-bs-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</button>
+<button class="button carousel-control-next"
+type="button"
+data-bs-target="#carouseltest"
+data-bs-slide="next"><span class="carousel-control-next-icon"></span></button>
+
+</div>
+</div>
+
+    </div>
+    
+   
+</div>
+</div>
+    <div class="container my-5">
+    <div>
+                <h1>Unleashing Hope:transforming Lives, One paw at a time<h1>
+                  
+             </div>
+</div>
+
+</div>
+<div class="container my-5">
+    <div>
+                <p>Welcome to the heartwarming world of ADUCats, together to create stories of 
+                    compassion, resilience, and second chances. Our mission is simple but profound
+                    to help homeless cats. Join us on this encredible journey as we celebrate the joy 
+                    of adoption and the extraordinary bond between humans and their four-legged friends.
+                </p>
+                  
+             </div>
+</div> 
+ 
+        
+</body>
+
+<style>
+    h1{
+        font-size: 50px;
+        
+    };
+    body{
+            background-color: #ff9966;
+        };
+        p{
+            font-size: 30px;
+        };
+        .container{
+       margin-top:100px;
+       margin-bottom:100px; 
+        };
+    
+</style>
