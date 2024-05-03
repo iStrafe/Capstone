a4ad9191bf869b607e49890094b9814aed76b3c1
iStrafe <Yugotabata@gmail.com>
2024-05-03 08:32:02 +0800

    updated report page

--- resources/views/Services/report.blade.php
+++ resources/views/Services/report.blade.php
@@ -7,7 +7,7 @@
 
     </style>
     <body class="..">
-        <div class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="font-sans antialiased dark:bg-black dark:text-white/50 border border-warning">
             @include('Navigationbar')
         </div>
 </head>
@@ -17,46 +17,48 @@
 <form method="POST" action="{{route('admin.report')}}">
                 @csrf
                 @method('POST')
-                <div>
+            <div class="container">
+                <div class="form-control">
                     <label for="">Name</label>
                     <input type="text" name="name" placeholder="name">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Gender</label>
                     <input type="text" name="gender" placeholder="gender">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Breed</label>
                     <input type="text" name="breed" placeholder="Breed">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Eye Color</label>
                     <input type="text" name="eye_color" placeholder="eye color">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Fur Color</label>
                     <input type="text" name="fur_color" placeholder="fur color">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Last seen date</label>
                     <input type="date" name="last_seen_date" placeholder="last seen date">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Last seen location</label>
                     <input type="text" name="last_seen_location" placeholder="last seen location">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Contact email</label>
                     <input type="email" name="contact_email" placeholder="contact email">
                 </div>
-                <div>
+                <div class="form-control">
                     <label for="">Upload image</label>
                     <input type="file" name="catimage" placeholder="upload image">
                 </div>
-                <div>
+                <div class="btn btn-outline-secondary">
                     <label for="">Submit</label>
                     <input type="submit" value="Save new info">
                 </div>
+            </div>
                     
             </form>
 
