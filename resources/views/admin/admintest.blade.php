a8c14caff0cc3b56c7b7850a530bf236d87ec23b
iStrafe <Yugotabata@gmail.com>
2024-05-04 18:38:11 +0800

    Nag lagay ako ng comments

--- resources/views/admin/admintest.blade.php
+++ resources/views/admin/admintest.blade.php
@@ -1,32 +1,12 @@
-
-  
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-       <h1>Create info</h1>
-
-        <div class="table-responsive">
-            <table class="table table-striped table-hover table-condensed">
-                    <tbody>
-                        @foreach($displayData as $key => $data)
-                        <tr>
-                            <th>{{$data->id}}</th>
-                            <th>{{$data->name}}</th>
-                            <th>{{$data->gender}}</th>
-                            <th>{{$data->breed}}</th>
-                            <th>{{$data->eye_color}}</th>
-                            <th>{{$data->fur_color}}</th>
-                            <th>{{$data->description}}</th>
-                            <th>{{$data->status}}</th>
-                        </tr>
-                        @endforeach
-                    </tbody>
-            </table>
-         </div>
-
-        
-         
-    </body>
-
-
-
-
-
+<!--<table border="1">
+            @foreach($displayData as $catinfo)
+            <tr>
+                <td>{{$catinfo->id}}</td>
+                <td>{{$catinfo->name}}</td>
+                <td>{{$catinfo->gender}}</td>
+                <td>{{$catinfo->eye_color}}</td>
+                <td>{{$catinfo->fur_color}}</td>
+                <td>{{$catinfo->description}}</td>
+                <td></td>
+            </tr>@endforeach
+        </table>-->
