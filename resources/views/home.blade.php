1b86f3e205c39a3d01510167973eadfb4f35b549
Ayet07 <thea07bartolazo@gmail.com>
2024-08-26 13:17:58 +0800

    gegegege

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,121 +1,128 @@
-<html>
-    <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdUCats</title>
     
-     <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-         <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        
-        
-
-        <!-- Styles -->
-        @include('scripts')
-      
-
-    </head>
-
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
     
-         <nav class="navbar navbar-expand-lg border-bottom border-body border border-warning" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand px-5 py-3" href="{{url('/')}}">HOME</a>
-                <a class="navbar-brand px-4 py-3" href="{{url('aboutus')}}">ABOUT US</a>
-                <a class="navbar-brand px-4 py-3" href="{{url('services')}}">SERVICES</a>
-                <a class="navbar-brand px-4 py-3" href="{{url('events')}}">NEWS / EVENTS</a>
-                <a class="navbar-brand px-4 py-3" href="ContactUs">CONTACT US</a>
-                <a class="navbar-brand px-4 py-3" href="#">FEED</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- Styles -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @include('scripts')
+
+    <style>
+        body {
+            font-family: 'Figtree', sans-serif;
+            background-color: #d0e7ff; /* Light blue background */
+            background-size: cover;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        .navbar {
+            background-color: #03045E; /* Adamson blue */
+            padding: 1rem;
+        }
+        .navbar a {
+            color: white;
+            margin-right: 1rem;
+        }
+        .navbar .dropdown-menu {
+            background-color: #003366;
+        }
+        .container {
+            background-color: rgba(255, 255, 255, 0.9);
+            padding: 20px;
+            border-radius: 10px;
+            margin: 20px auto;
+            width: 30%;
+            max-width: 1200px;
+            text-align: center; /* Center-align text inside the container */
+        }
+        .logo {
+            max-width: 150px;
+            margin: 0 auto 20px; /* Center the logo and add space below */
+            display: block;
+        }
+        .center {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .input-group {
+            width: 100%;
+            max-width: 500px; /* Adjust the maximum width of the input group */
+        }
+    </style>
+</head>
+
+<body class="font-sans antialiased">
+    <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand px-5 py-3" href="{{ url('/') }}">HOME</a>
+            <a class="navbar-brand px-4 py-3" href="{{ url('aboutus') }}">ABOUT US</a>
+            <a class="navbar-brand px-4 py-3" href="{{ url('services') }}">SERVICES</a>
+            <a class="navbar-brand px-4 py-3" href="{{ url('events') }}">NEWS / EVENTS</a>
+            <a class="navbar-brand px-4 py-3" href="{{ url('ContactUs') }}">CONTACT US</a>
+            <a class="navbar-brand px-4 py-3" href="#">FEED</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <!--In between navbar items and dropdown menu-->
-                </ul>
-
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
                 <form class="d-flex px-5" role="search">
-                     @if (Route::has('login'))
-                            <nav class="-mx-3 flex flex-1 justify-end ">
-                                @auth
-                                   <!--Dropdown menu-->
-                                    <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                             <div>{{ Auth::user()->name }}</div>
-                                             
-                                        </button>
-                                        
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                                <x-dropdown-link :href="route('profile.edit')" class="dropdown-item">
-                                                {{ __('Profile') }}
-                                                </x-dropdown-link>
-
-                                            <form method="POST" action="{{ route('logout') }}">
-                                                @csrf
-                                                <x-dropdown-link :href="route('logout')" class="dropdown-item"
-                                                        onclick="event.preventDefault();
-                                                                    this.closest('form').submit();">
-                                                    {{ __('Log Out') }}
-                                                </x-dropdown-link>
-                                            </form>
-                                        </div>
-                                    </div>  <!--Dropdown menu end-->
-                                @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-5 py-3 text-white ring-1 ring-transparent transition hover:text-White/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/100 dark:focus-visible:ring-white"
-                                    >
-                                        Log in
+                    @if (Route::has('login'))
+                        <nav class="-mx-3 flex flex-1 justify-end">
+                            @auth
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <div>{{ Auth::user()->name }}</div>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <x-dropdown-link :href="route('profile.edit')" class="dropdown-item">
+                                            {{ __('Profile') }}
+                                        </x-dropdown-link>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <x-dropdown-link :href="route('logout')" class="dropdown-item" onclick="event.preventDefault(); this.closest('form').submit();">
+                                                {{ __('Log Out') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                    </div>
+                                </div>
+                            @else
+                                <a href="{{ route('login') }}" class="rounded-md px-5 py-3 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/100 dark:focus-visible:ring-white">
+                                    Log in
+                                </a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="rounded-md px-5 py-3 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                        Register
                                     </a>
-
-                                    @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-5 py-3 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
-                                        </a>
-                                    @endif
-                                @endauth
-                                <div class="dropdown show">
-                            </nav>
-                        @endif
-                    </ul>
+                                @endif
+                            @endauth
+                        </nav>
+                    @endif
                 </form>
-                </div>
             </div>
-        </nav>
-
-
-        <div class="border border-warning">
-            <img src="images\Cat2.jpg"  style="width:100%; height: 500px;">
         </div>
-
-        <section class="bg-blacks">
-            <div class="container">
-                <div class="center">
-                 <h1>SEARCH CAT</h1>
-                    <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Search Cat" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="button">Advanced Search</button>
-                        </div>
+    </nav>
+    <br><br><br><br><br>
+    <section>
+        <div class="container">
+            <img src="images/logo.png" alt="Logo" class="logo">
+            <div class="center">
+                <h1>SEARCH CAT</h1>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search Cat" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button">Advanced Search</button>
                     </div>
                 </div>
             </div>
-        </section>
-
-        
-        
-    </body>
-
-
-    
+        </div>
+    </section>
+</body>
 </html>
-
-
