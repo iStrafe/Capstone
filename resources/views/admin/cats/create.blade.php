8ce2f8f073f0f7b3c8f8ff7a6908363e985fd67a
iStrafe <Yugotabata@gmail.com>
2024-10-06 18:19:05 +0800

    CRUD for cat info into modal

--- resources/views/admin/cats/create.blade.php
+++ resources/views/admin/cats/create.blade.php
@@ -1,66 +1,66 @@
-@include('scripts')
-@include('admin.adminNavbar')
-<!DOCTYPE html>
-    <title>Add Cat</title>
-        <body>
-    @if ($errors->any())
-        <div class="alert alert-danger">
+<!-- Modal -->
+<div class="modal fade" id="addCatModal" tabindex="-1" aria-labelledby="addCatModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-center" id="addCatModalLabel" style="font-size: 2rem;">Add Cat</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        @if ($errors->any())
+          <div class="alert alert-danger">
             <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
             </ul>
-        </div>
-    @endif
+          </div>
+        @endif
 
-    <form action="{{ route('admin.cats.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
+        <form action="{{ route('admin.cats.store') }}" method="POST" enctype="multipart/form-data">
+          @csrf
 
-        <div class="container-sm">
+          <div class="container-sm">
+            <div class="mb-3">
+              <label for="cat_name" class="form-label">Cat Name</label>
+              <input type="text" name="cat_name" class="form-control" required>
+            </div>
 
             <div class="mb-3">
-            <label for="cat_name" class="form-label">Cat Name</label>
-            <input type="text" name="cat_name" class="form-control" required>
+              <label for="cat_image" class="form-label">Cat Image</label>
+              <input type="file" name="cat_image" class="form-control">
             </div>
+
+            <div class="mb-3">
+              <label for="age" class="form-label">Age</label>
+              <input type="number" name="age" class="form-control" required>
+            </div>
+
+            <div class="mb-3">
+              <label for="color" class="form-label">Color</label>
+              <input type="text" name="color" class="form-control" required>
+            </div>
+
+            <div class="mb-3">
+              <label for="breed" class="form-label">Breed</label>
+              <input type="text" name="breed" class="form-control" required>
+            </div>
+
             
-            <div class="mb-3">
-            <label for="cat_image" class="form-label">Cat Image</label>
-            <input type="file" name="cat_image" class="form-control">
-            </div>
-            
-            <div class="mb-3">
-            <label for="age" class="form-label">Age</label>
-            <input type="number" name="age" class="form-control" required>
-            </div>
 
             <div class="mb-3">
-            <label for="color" class="form-label">Color</label>
-            <input type="text" name="color" class="form-control" required>
-            </div>
-
-            <div class="mb-3">
-            <label for="breed" class="form-label">Breed</label>
-            <input type="text" name="breed" class="form-control" required>
-            </div>
-
-            <div class="mb-3">
-            <label for="sex" class="form-label">Sex</label>
-            <select name="sex" class="form-select" required>
+              <label for="sex" class="form-label">Sex</label>
+              <select name="sex" class="form-select" required>
                 <option selected>Open this select menu</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
-            </select>
+              </select>
             </div>
 
-            <button type="submit" class="btn btn-primary mb-3">Add Cat</button>
-
-        </div>
-
-        
-        <br>
-        
-
-        
-    </form>
-        </body>
-</html>
+            <button type="submit" class="btn btn-primary mb-3" style="color: black;">Add Cat</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/admin/cats/index.blade.php
+++ resources/views/admin/cats/index.blade.php
@@ -1,48 +1,54 @@
 @include('scripts')
 @include('admin.adminNavbar')
 <!DOCTYPE html>
-    <title>Cats list</title>
-        <body>
+<title>Cats list</title>
+<body>
 
-    <h1>Cat Adoption List</h1>
-    <button type="button" class="btn btn-success"><a href="{{ route('admin.cats.create') }}">Add New Cat</a></button>
-    
-    <table class="table table-hover">
+<h1>Cat Adoption List</h1>
+<button type="button" class="btn btn-success text-white" data-bs-toggle="modal" data-bs-target="#addCatModal">Add New Cat</button>
+
+<table class="table table-hover">
+    <tr>
+        <th>Cat Name</th>
+        <th>Cat Image</th>
+        <th>Age</th>
+        <th>Color</th>
+        <th>Breed</th>
+        <th>Sex</th>
+        <th>Actions</th>
+    </tr>
+    @foreach($cats as $cat)
         <tr>
-            <th>Cat Name</th>
-            <th>Cat Image</th>
-            <th>Age</th>
-            <th>Color</th>
-            <th>Breed</th>
-            <th>Sex</th>
-            <th>Actions</th>
+            <td>{{ $cat->cat_name }}</td>
+            <td>
+                @if($cat->cat_image)
+                    <img src="{{ asset('images/' . $cat->cat_image) }}" alt="Image of {{ $cat->cat_name }}" style="width: 50px; height: auto;">
+                @else
+                    No image
+                @endif
+            </td>
+            <td>{{ $cat->age }}</td>
+            <td>{{ $cat->color }}</td>
+            <td>{{ $cat->breed }}</td>
+            <td>{{ $cat->sex }}</td>
+            <td>
+                <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#showCatModal">View</button>
+                <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#editCatModal">Edit</button>
+                
+                
+                <form action="{{ route('admin.cats.destroy', $cat->id) }}" method="POST" style="display:inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-link">Delete</button>
+                </form>
+            </td>
         </tr>
-        @foreach($cats as $cat)
-            <tr>
-                <td>{{ $cat->cat_name }}</td>
-                <td>
-                    @if($cat->cat_image)
-                        <img src="{{ asset('images/' . $cat->cat_image) }}" alt="Image of {{ $cat->cat_name }}" style="width: 50px; height: auto;">
-                    @else
-                        No image
-                    @endif
-                </td>
-                <td>{{ $cat->age }}</td>
-                <td>{{ $cat->color }}</td>
-                <td>{{ $cat->breed }}</td>
-                <td>{{ $cat->sex }}</td>
-                <td>
-                <button type="button" class="btn btn-link"><a href="{{ route('admin.cats.show', $cat->id) }}">View</a> </button>
-                <button type="button" class="btn btn-link"><a href="{{ route('admin.cats.edit', $cat->id) }}">Edit</a> </button>
-                    
-                    <form action="{{ route('admin.cats.destroy', $cat->id) }}" method="POST" style="display:inline">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-link">Delete</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-        </body>
+    @endforeach
+</table>
+
+@include('admin.cats.create') 
+@include('admin.cats.edit')
+@include('admin.cats.show') 
+
+</body>
 </html>
